bd0050ef0352aac491deab240df8da491b690cda
Qoo <myungthe@naver.com>
2017-09-08 22:07:17 +0900

    최초 코드

---
+++ 4_Buffers_Shaders_and_HLSL/4_Buffers_Shaders_and_HLSL/InputClass.cpp
@@ -0,0 +1,49 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: inputclass.cpp
+////////////////////////////////////////////////////////////////////////////////
+#include "InputClass.h"
+
+InputClass::InputClass()
+{
+}
+
+InputClass::InputClass(const InputClass& ohter)
+{
+}
+
+InputClass::~InputClass()
+{
+}
+
+void InputClass::Initialize()
+{
+	int i;
+
+	// 모든 키들을 눌리지 않은 상태로 초기화합니다.
+	for (i = 0; i < 256; ++i)
+	{
+		m_keys[i] = false;
+	}
+
+	return;
+}
+
+void InputClass::KeyDown(unsigned int input)
+{
+	// 키가 눌렸다면 그 상태를 배열에 저장합니다.
+	m_keys[input] = true;
+	return;
+}
+
+void InputClass::KeyUp(unsigned int input)
+{
+	// 키가 떼어졌다면 그 상태를 배열에 저장합니다.
+	m_keys[input] = false;
+	return;
+}
+
+bool InputClass::IsKeyDown(unsigned int key)
+{
+	// 현재 키가 눌림/뗌 상태인지 반환합니다.
+	return m_keys[key];
+}
